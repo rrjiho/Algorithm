e1b26820cc3c5381eb540b5b1e724f5109be6cd4
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-09-10 19:52:39 +0900

    Create 배열 원소의 길이.cs

---
+++ 프로그래머스/Lv. 0/배열 원소의 길이.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] strlist)
+    {
+        int[] answer = new int[strlist.Length];
+
+        for(int i = 0; i < strlist.Length; i++)
+        {
+            answer[i] = strlist[i].Length;
+        }
+
+        // int[] answer = strlist.Select(x => x.Length).ToArray();
+      
+        return answer;
+    }
+}
