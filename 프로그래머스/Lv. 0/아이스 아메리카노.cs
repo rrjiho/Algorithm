36025f390b8d9f738013e4887f3c2322d12318eb
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-09-10 09:33:09 +0900

    Create 아이스 아메리카노.cs

---
+++ 프로그래머스/Lv. 0/아이스 아메리카노.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int[] solution(int money) {
+        
+        int[] answer = new int[2] {money / 5500, money % 5500};
+        
+        return answer;
+    }
+}
