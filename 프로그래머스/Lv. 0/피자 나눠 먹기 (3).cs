b717af476c5b1b7e70c388e70a671f9b3539f114
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-09-10 19:48:24 +0900

    Create 피자 나눠 먹기 (3).cs

---
+++ 프로그래머스/Lv. 0/피자 나눠 먹기 (3).cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        
+        if(n % slice == 0)
+            answer = n / slice;
+        else
+            answer = n / slice + 1;
+
+        // answer = n % slice == 0 ? n / slice : n / slice + 1;
+      
+        return answer;
+    }
+}
