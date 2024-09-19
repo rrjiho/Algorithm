8307ea6f58a3b1d45822f039ca8153bc2a5a4e18
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-09-19 15:23:41 +0900

    Update and rename Test.cs to 평균 구하기.cs

---
+++ 프로그래머스/Lv. 1/평균 구하기.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+
+public class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        
+        answer = arr.Average();
+        
+        return answer;
+    }
+}
