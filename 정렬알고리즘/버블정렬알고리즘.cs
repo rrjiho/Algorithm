8b7c966aa5d9bf04121dcbb0203361830c0c0be0
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-09-24 13:32:20 +0900

    Create 버블정렬알고리즘.cs

---
+++ 정렬알고리즘/버블정렬알고리즘.cs
@@ -0,0 +1,20 @@
+class Program
+{
+    static void BubbleSort(int[] arr)
+  {
+      int n = arr.Length;
+  
+      for (int i = 0; i < n - 1; i++)
+      {
+          for (int j = 0; j < n - i - 1; j++)
+          {
+              if (arr[j] > arr[j + 1])
+              {
+                  int temp = arr[j];
+                  arr[j] = arr[j + 1];
+                  arr[j + 1] = temp;
+              }
+          }
+      }
+  }
+} // 단순히 하나씩 비교해가며 자리를 바꾸는 알고리즘
