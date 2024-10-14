615faaf048d791505138d32a3c6bf2b413824f64
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-10-14 15:09:47 +0900

    Create 숫자 문자열과 영단어.cs

---
+++ 프로그래머스/Lv. 1/숫자 문자열과 영단어.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution 
+{
+    public int solution(string s) 
+    {
+        string[] num = new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i = 0; i < 10; i++)
+        {        
+            s = s.Replace(num[i], i.ToString());
+        }
+
+        return Int32.Parse(s);
+    }
+}
