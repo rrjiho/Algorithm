fb49445abf615cbd2f1bf5212c7cbe248536efdd
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-11-04 15:39:43 +0900

    Create 소문자로 바꾸기.cs

---
+++ 프로그래머스/Lv. 0/소문자로 바꾸기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = "";
+        
+        answer = myString.ToLower();
+        
+        return answer;
+    }
+}
