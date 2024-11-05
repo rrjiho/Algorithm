fda78fbd41dbc2a51ef14bbe44d7b181ac87aa77
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-11-05 10:16:59 +0900

    Create 접미사인지 확인하기.cs

---
+++ 프로그래머스/Lv. 0/접미사인지 확인하기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(string my_string, string is_suffix) {
+        return my_string.EndsWith(is_suffix) ? 1 : 0;
+    }
+}
+// System.string에 다양한 메서드를 이미 만들어 놓았기 때문에
+// 알고 사용하는 것 또한 역량임을 알아야 함
