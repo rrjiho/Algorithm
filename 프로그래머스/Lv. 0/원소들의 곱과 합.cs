823a5bb8524e3e13db45c67942f3731221da87e0
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-11-12 17:07:36 +0900

    Create 원소들의 곱과 합.cs

---
+++ 프로그래머스/Lv. 0/원소들의 곱과 합.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+        for(int i = 0; i < num_list.Length; i++){
+            sum += num_list[i];
+            mul *= num_list[i];
+        }
+        if(sum * sum > mul){
+            answer = 1;
+        }
+        return answer;
+    }
+}
