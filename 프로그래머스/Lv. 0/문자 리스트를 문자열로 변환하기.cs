93d3233f9667d4cd860c633dea059e5844412414
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-11-15 22:00:29 +0900

    Create 문자 리스트를 문자열로 변환하기.cs

---
+++ 프로그래머스/Lv. 0/문자 리스트를 문자열로 변환하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public string solution(string[] arr) {
+        
+        string answer = "";
+        
+        foreach (string c in arr)
+        {
+            answer += c;
+        }
+        
+        return answer;
+    }
+}
