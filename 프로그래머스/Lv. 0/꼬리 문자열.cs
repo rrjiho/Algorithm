ed0ceb5e60948a8995ef7f64680da843596e6bae
Cookies <76803056+rrjiho@users.noreply.github.com>
2024-11-22 22:51:15 +0900

    Create 꼬리 문자열.cs

---
+++ 프로그래머스/Lv. 0/꼬리 문자열.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(string[] str_list, string ex) {
+        string answer = "";
+        
+        foreach (string str in str_list)
+        {
+            if (!str.Contains(ex))
+            {
+                answer += str;
+            }
+        }
+        
+        return answer;
+    }
+}
